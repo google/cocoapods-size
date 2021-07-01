d9f6a3ed327d50369eee5742ab3e1f289c8cba60
Gran <granluo@google.com>
2021-07-01 15:53:03 -0700

    Update measure_cocoapod_size.py

--- measure_cocoapod_size.py
+++ measure_cocoapod_size.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/usr/bin/python
 """measure_cocoapod_size.py provides size impact of a given set of cocoapods.
 
 Usage: ./measure_cocoapod_size.py -cocoapods $POD_NAME:$POD_VERSION
